3d025642fcca6dfd6d60f6f93c0c065e0d9774b8
David Gutierrez <dgutierrez06@gmail.com>
2016-10-11 00:54:54 -0600

    Torres de Hanoi
    v1.1

---
+++ V1.1/GUI_Hanoi/GUI_Hanoi/GUI_Principal.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace GUI_Hanoi
+{
+    public partial class GUI_Principal : Form
+    {
+        public GUI_Principal()
+        {
+            InitializeComponent();
+        }
+    }
+}
